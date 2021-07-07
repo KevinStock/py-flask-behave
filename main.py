c6868260eea8f3dc795c426931b0ca0bdf0ef6d4
Kevin Stock <kstock@uncomn.com>
2021-07-07 12:57:07 -0500

    add zipcode distance route and functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import flask
+import requests
 from flask import jsonify
 
 app = flask.Flask(__name__)
@@ -12,6 +13,17 @@ def hello(name):
     }
     return jsonify(message)
 
+@app.route('/distance/<frm>/<to>', methods=['GET'])
+def distance(frm, to):
+    apiKey = "EgCY4hR3CMStzP5taEAT4xRT0cMW392a18rKTZQjNV7YHjdrpaQaiIeMtHshpYsr"
+    zipCodeURL = 'https://www.zipcodeapi.com/rest/'
+
+    req = requests.get(zipCodeURL + apiKey + '/distance.json/' + frm + '/' + to + '/mile')
+    if (req.status_code == 200):
+        return req.json()
+    else:
+        print(req.status_code + req.text)
+        return jsonify({distance: -1})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
